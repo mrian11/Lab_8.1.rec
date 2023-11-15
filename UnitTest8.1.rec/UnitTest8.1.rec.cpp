dae7c0567056762d22437e54ed74edf448b828f3
mrian11 <marian.yatsyk.ri.2023@lpnu.ua>
2023-11-15 15:31:00 +0200

    Lab_8.1.rec

---
+++ UnitTest8.1.rec/UnitTest8.1.rec.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../Lab_8.1.rec/LAB.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81rec
+{
+	TEST_CLASS(UnitTest81rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(Count("d,-g..g,-", 0), 2);
+		}
+	};
+}
